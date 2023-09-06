cb3d35d7d13e2d0e5a2758f8b99647783c3c040e
Edgardo Alvarez <edgardo@livedesign.org>
2023-09-06 17:26:57 -0500

    “update”

--- src/store/apis/todosApi.js
+++ src/store/apis/todosApi.js
@@ -5,11 +5,14 @@ export const todosApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://jsonplaceholder.typicode.com",
   }),
-  endpoints: (builder) => {
+  endpoints: (builder) => ({
     getTodos: builder.query({
-      query: () => "todos",
-    });
-  },
+      query: () => "/todos",
+    }),
+    getTodo: builder.query({
+      query: (todoId) => `/todos/${todoId}`,
+    }),
+  }),
 });
 
-export const { useGetTodosQuery } = todosApi;
+export const { useGetTodosQuery, useGetTodoQuery } = todosApi;
